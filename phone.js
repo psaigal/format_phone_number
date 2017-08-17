400d053f8e05d4b21e923ea8f97178f9fcd5c98a
psaigal <psaigal48@gmail.com>
2017-08-17 14:45:39 -0700

    Add function that formats phone number

--- phone.js
+++ phone.js
@@ -3,7 +3,23 @@ document.addEventListener("DOMContentLoaded", function(){
 	inputPhone.addEventListener("keypress", formatPhoneNumberCB)
 
 	function formatPhoneNumberCB(event) {
-		var number = event.target.number;
-		
+		var number = event.target.value;
+		if (number.length > 13) {
+			event.preventDefault();
+			return false;
+		}
+		var newNumber = number[number.length-1];
+		if (number.length === 1) {
+			number = "(" + newNumber;
+		}
+		else if (number.length === 4) {
+			number += ") ";
+		}
+		else if (number.length === 9) {
+			number += "-";
+		}
+		inputPhone.value = number;
 	}
 })
+
+
